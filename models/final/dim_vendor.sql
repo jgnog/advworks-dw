03d5284fbf6b2a86681f9eca1d5fe7916d8ae875
Yuri Freitas <yurimfreitas@gmail.com>
2024-05-15 20:29:59 +0000

    final

--- models/final/dim_vendor.sql
+++ models/final/dim_vendor.sql
@@ -1,40 +1,64 @@
--- dim_vendor	 
-WITH numbered_contacts AS (
-    SELECT v.businessentityid AS vendor_id,
-           v.accountnumber as account_number,
-           v.name as vendor_name,
-           v.creditrating as credit_rating,
-           v.preferredvendorstatus as preferred_vendor_status,
-           v.activeflag as active_flag,
-           v.purchasingwebserviceurl as purchasing_web_service_url,
-           ct.name AS contact_type,
-           row_number() OVER (PARTITION BY v.businessentityid ORDER BY ct.name) AS row_nr_contact,
-           p.title,
-           p.firstname as fisrt_name,
-           p.middlename as middle_name,
-           p.lastname as last_name,
-           p.suffix,
-           pp.phonenumber as phone_number,
-           pnt.name AS phone_number_type,
-           ea.emailaddress as email_address,
-	       vw_adresses.addresstype as address_type,
-	       vw_adresses.addressline1 as address_line1,
-	       vw_adresses.addressline2 as address_line2,
-	       vw_adresses.city,
-	       vw_adresses.stateprovincename as state_province_name,
-	       vw_adresses.postalcode as postal_code,
-	       vw_adresses.countryregionname as country_region_name,
-           v.modifieddate as modified_date
-    FROM purchasing.vendor v
-    JOIN person.businessentitycontact bec ON bec.businessentityid = v.businessentityid
-    JOIN person.contacttype ct ON ct.contacttypeid = bec.contacttypeid
-    JOIN person.person p ON p.businessentityid = bec.personid
-    LEFT JOIN person.emailaddress ea ON ea.businessentityid = p.businessentityid
-    LEFT JOIN person.personphone pp ON pp.businessentityid = p.businessentityid
-    LEFT JOIN person.phonenumbertype pnt ON pnt.phonenumbertypeid = pp.phonenumbertypeid
-	LEFT JOIN purchasing.vvendorwithaddresses vw_adresses ON vw_adresses.businessentityid = v.businessentityid
+with first_version as ( 
+    SELECT dbt_scd_id, 
+            vendor_id,
+            account_number,
+            vendor_name,
+            credit_rating,
+            preferred_vendor_status,
+            active_flag,
+            purchasing_web_service_url,
+            contact_type,
+            row_nr_contact,
+            title,
+            fisrt_name,
+            middle_name,
+            last_name,
+            suffix,
+            phone_number,
+            phone_number_type,
+            email_address,
+            address_type,
+            address_line1,
+            address_line2,
+            city,
+            state_province_name,
+            postal_code,
+            country_region_name,
+            dbt_valid_from,
+            dbt_valid_to,
+            dbt_updated_at,
+            row_number() over(partition by vendor_id order by dbt_valid_from) as row_nr
+    FROM {{ ref('sp_vendor') }}
 )
-SELECT *
-FROM numbered_contacts
-WHERE row_nr_contact = 1
-ORDER BY vendor_id
+SELECT dbt_scd_id as sk_shipment,
+       vendor_id,
+       account_number,
+       vendor_name,
+       credit_rating,
+       preferred_vendor_status,
+       active_flag,
+       purchasing_web_service_url,
+       contact_type,
+       row_nr_contact,
+       title,
+       fisrt_name,
+       middle_name,
+       last_name,
+       suffix,
+       phone_number,
+       phone_number_type,
+       email_address,
+       address_type,
+       address_line1,
+       address_line2,
+       city,
+       state_province_name,
+       postal_code,
+       country_region_name,
+       case
+           when row_nr = 1 then '1970-01-01'
+           else dbt_valid_from
+       end as valid_from,
+       coalesce(dbt_valid_to, '2200-01-01') as valid_to,
+       dbt_updated_at as last_updated_at
+from first_version
--- models/final/fact_purchase.sql
+++ models/final/fact_purchase.sql
@@ -1,21 +1,45 @@
--- fact_purchase
-select po.purchaseorderid as purchaseorder_id,
-       po.revisionnumber as revision_number,
-	   CASE WHEN po.status = 1 THEN 'Pending'
-	        WHEN po.status = 2 THEN 'Approved'
-			WHEN po.status = 3 THEN 'Rejected'
-			WHEN po.status = 4 THEN 'Complete'
-		END as status,
-       e.businessentityid as employee_id,
-	   v.businessentityid as vendor_id,
-	   s.shipmethodid as ship_method_id,
-	   po.orderdate as order_date,
-	   po.shipdate as ship_date,
-	   po.subtotal as sub_total,
-	   po.taxamt as tax_amount,
-	   po.freight,
-	   po.modifieddate as modified_date
-  from purchasing.purchaseorderheader po
-       inner join humanresources.employee e on e.businessentityid = po.employeeid
-	   inner join purchasing.vendor v on v.businessentityid = po.vendorid
-	   inner join purchasing.shipmethod s on s.shipmethodid = po.shipmethodid
+with surrogate_keys as (
+    select
+        o.purchaseorder_id,
+        o.revision_number,
+        o.status,
+        demp.sk_employee as sk_employee,
+        dv.sk_vendor as sk_vendor,
+        dship.sk_shipment as sk_shipment,
+        order_dd.sk_date as sk_order_date,
+        o.ship_date,
+        date_part('day', o.ship_date::timestamp - o.order_date::timestamp) as time_to_ship_days,
+        o.sub_total,
+        o.tax_amount,
+        o.freight,
+        o.modified_date
+    from {{ ref('stg_purchasing') }} o
+        join {{ ref('dim_date') }} p_dd on o.order_date = p_dd.date
+        left join {{ ref('dim_vendor') }} dv on dv.vendor_id = o.vendor_id
+            and o.order_date between vd.valid_from and dv.valid_to
+        left join {{ ref('dim_shipment') }} dship on dship.ship_method_id = o.ship_method_id
+            and o.order_date between dship.valid_from and dship.valid_to
+        left join {{ ref('dim_employee') }} demp on demp.employee_id = o.employee_id
+            and o.order_date between demp.valid_from and demp.valid_to
+),
+final as (
+    select
+        purchaseorder_id,
+        revision_number,
+        status,
+        sk_employee,
+        sk_vendor,
+        sk_shipment,
+        sk_order_date,
+        ship_date,
+        time_to_ship_days,
+        sub_total,
+        tax_amount,
+        freight,
+        modified_date
+    from surrogate_keys
+)
+
+select
+    *
+from final
