b7f1165755a5d21b79859bd388667295089c7bd1
Tiago Costa <tiaguinhuh_costa@hotmail.com>
2024-05-18 19:28:18 +0000

    Stg_employee

--- models/staging/stg_employee.sql
+++ models/staging/stg_employee.sql
@@ -1,2 +1,11 @@
-select nationalidnumber
-from {{ source ('humanresources', 'employee') }}
+SELECT
+	per.businessentityid,
+	per.firstname,
+	per.lastname,
+    e.nationalidnumber, 
+    e.gender, 
+    s.name
+from {{ source("humanresources", "employee") }} e
+    join {{ source("humanresources", "employeedepartmenthistory") }} edh on e.businessentityid = edh.businessentityid
+    join {{ source("humanresources", "shift") }} s on edh.shiftid = s.shiftid
+    join {{ source("person", "person") }} per on edh.businessentityid = per.businessentityid
